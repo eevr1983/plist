a87882b10cea36acbb7fd3274a9707d3b23e24de
evgeny <e-e-v@mail.ru>
2021-12-07 22:28:43 +0300

    Пытался прописать роуты

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function index() {
+        return view('base');
+    }
+
+    public function waybill() {
+        return view('waybill');
+    }
+
+    public function contract() {
+        return view('contract');
+    }
+
+    public function freighter_card() {
+        return view('freighter_card');
+    }
+
+    public function memo() {
+        return view('memo');
+    }
+
+    public function cars() {
+        return view('cars');
+    }
+
+    public function employees() {
+        return view('employees');
+    }
+
+    public function organizations() {
+        return view('organizations');
+    }
+
+    public function settings() {
+        return view('settings');
+    }
+
+    public function support() {
+        return view('support');
+    }
+
+    public function dashboard() {
+        return view('dashboard');
+    }
+
+    public function car($car = null) {
+        dd($car);
+        return view('base', ['car' => $car]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('base');
-});
+Route::get('/', [MainController::class, 'index']);
+Route::get('/dashboard', [MainController::class, 'dashboard']);
+Route::get('/waybill', [MainController::class, 'waybill']);
+Route::get('/contract', [MainController::class, 'contract']);
+Route::get('/freighter_card', [MainController::class, 'freighter_card']);
+Route::get('/memo', [MainController::class, 'memo']);
+Route::get('/cars', [MainController::class, 'cars']);
+Route::get('/employees', [MainController::class, 'employees']);
+Route::get('/organizations', [MainController::class, 'organizations']);
+Route::get('/settings', [MainController::class, 'settings']);
+Route::get('/support', [MainController::class, 'support']);
+Route::get('/cars/{ car? }', [MainController::class, 'car']);
